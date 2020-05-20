136c42bb9907bcf8f905b32a77811ca90c727a1a
animeshp <animeshp@INLT957.sapiens.int>
2020-05-20 17:13:50 +0530

    Retail Server First Commit

---
+++ src/test/java/com/retailServer/retail/server/RetailServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailServer.retail.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
